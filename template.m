8b3c45703499a645c75feaee6225fafd4530c6ce
Ally Hume <a.hume@ed.ac.uk>
2017-03-10 15:40:56 +0000

    Update template.m

--- template.m
+++ template.m
@@ -1,5 +1,5 @@
 resultFileName = 'myResultFile.csv';
-addpath('../../frameworkmodel_dev');
-addpath('../../frameworkmodel_dev/PIF_CO_FT_model')
+addpath('../../frameworkmodel');
+addpath('../../frameworkmodel/PIF_CO_FT_model')
 run_simulation(resultFileName,tempDay,tempNight,tempDayLength,tempTwilight,co2Day,co2Night,co2DayLength,co2Twilight,lightDay,lightNight,lightDayLength,lightTwilight,clockGenotype)
 
